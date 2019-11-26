2d2d42124d0614050e64a7f1c11730e712a8aa95
zroykhi <727125137@qq.com>
2019-11-26 14:22:05 +0100

    udpate

--- DIMACSparser.py
+++ DIMACSparser.py
@@ -1,12 +1,24 @@
 filename = "simple.cnf"
-with open(filename,"r") as dimacs:
-    data = dimacs.readlines()
-    F = []
-    for element in data:
-        element.replace('\n', '')
-    for element in data:
-        if element.startswith('p'):
-            for k in range(int(element[-1])):
-                F.append(dict())
 
-# must return F = [{'1':None, '-3':None},{...} ]
+def DIMACSParser(filename):
+    with open(filename,"r") as dimacs:
+        lines = dimacs.readlines()
+        F = []
+        lines = [l.replace('\n', '').split() for l in lines]
+        start_flag = False
+        num_clause = 0
+        for line in lines:
+            if line[0] == 'p':
+                start_flag = True
+                num_clause = int(line[-1])
+            # don't count a comment row ends with 0, number of clause must be consistant
+            if line[-1] == '0' and start_flag and len(F) < num_clause:
+                tmp = {}
+                for i in range(len(line) - 1):
+                    tmp[int(line[i])] = None
+                F.append(tmp)
+        return F
+
+if __name__ == "__main__":
+    f = DIMACSParser(filename)
+    print(f)
