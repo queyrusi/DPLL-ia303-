51cb4e414990bf2f7d3e106970c467ff13f7fbf4
queyrusi <simon.queyrut@ensta-bretagne.org>
2019-11-26 12:17:11 +0100

    main

---
+++ dpll_main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#  -*- coding: utf-8 -*-
+
+"""dpll_main.py
+Functions and main for a dictionary-based-implementation of DPLL.
+
+Check https://github.com/queyrusi/DPLL-ia303- for updates!
+"""
+
+def unit_propagate(formula, model):
+    """
+
+    Args:
+        formula ():
+        model ():
+
+    Returns:
+
+    """
+    return
+
