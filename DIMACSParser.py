fda8968fd190e0649b18e749e92c90baeb2a1880
queyrusi <simon.queyrut@ensta-bretagne.org>
2019-12-10 11:37:44 +0100

    DPLL (needs to be cleant a bit)

--- DIMACSParser.py
+++ DIMACSParser.py
@@ -1,29 +1,35 @@
-filename = "simple.cnf"
+import sys
+
+filename = sys.argv[1]
 
 def read_file_to_list(filename):
     with open(filename,"r") as dimacs:
         lines = dimacs.readlines()
         lines = [l.replace('\n', '').split() for l in lines]
-        return lines
-    
+        clauses = []
+        tmp = None
+        for line in lines:
+            if tmp is not None:
+                line = tmp + line
+                tmp = None
+            if line[0] != 'c' and line[0] != 'p' and line[-1] != '0':
+                tmp = line
+            else:
+                clauses.append(line)
+        return clauses
+
 def DIMACSParser(lines):
+    F_str = [item[0:-1] for item in lines if (('c' not in item) and ('p' not in item))]
     F = []
-    start_flag = False
-    num_clause = 0
-    for line in lines:
-        if line[0] == 'p':
-            start_flag = True
-            num_clause = int(line[-1])
-        # don't count a comment row ends with 0, number of clause must be consistant
-        if (line[-1] == '0' or line[-1] == 0) and start_flag and len(F) < num_clause:
-            tmp = {}
-            for i in range(len(line) - 1):
-                tmp[int(line[i])] = None
-            F.append(tmp)
+    clause = []
+    for str_clause in F_str:
+        for literal in str_clause:
+            clause.append(int(literal))
+        F.append(clause)
+        clause = []
     return F
 
 if __name__ == "__main__":
     lines = read_file_to_list(filename)
-    print(lines)
     f = DIMACSParser(lines)
     print(f)
